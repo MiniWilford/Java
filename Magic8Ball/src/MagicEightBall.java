17aaeb087e8f5c284efff1040e4f4aea279d4c4a
MiniWilford <dejarnettkyle@yahoo.com>
2022-06-16 21:40:08 -0400

    Adding main components

---
+++ Magic8Ball/src/MagicEightBall.java
@@ -0,0 +1,13 @@
+// Magic 8 Ball Program, simple logic test
+import java.util.random.*; //random for more 'true' randomness
+import java.util.Scanner; //scanner for input from user
+
+public class MagicEightBall { 
+    public static void main(String[] args) {
+        //Welcome!
+        System.out.println("Welcome to Magic 8 Ball!\n");
+
+        // Ask user for what they want to ask for the 8 ball then use a Switch statement to determine 1-8 response. 
+        // Side-note: Possibly add a while(true) statement / if statement, possible for() could be faster for build time.
+    }
+}
---
+++ TestingJava/src/TestingJava.java
@@ -0,0 +1,7 @@
+public class TestingJava {
+
+    public static void main(String[] args) {        
+        
+        System.out.println("Test");
+    }
+}
