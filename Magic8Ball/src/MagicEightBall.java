bd6427e5847956ae782fb5bb6a4578f0f039d672
MiniWilford <dejarnettkyle@yahoo.com>
2022-06-22 20:38:52 -0400

    Adding boolean-type / while-statement to base code

--- Magic8Ball/src/MagicEightBall.java
+++ Magic8Ball/src/MagicEightBall.java
@@ -9,5 +9,17 @@ public class MagicEightBall {
 
         // Ask user for what they want to ask for the 8 ball then use a Switch statement to determine 1-8 response. 
         // Side-note: Possibly add a while(true) statement / if statement, possible for() could be faster for build time.
+        
+        boolean running = true; // true if program is running, false if user exits program.
+        while(running) {
+            //Ask user if they wish to continue
+            System.out.println("Would you like to play 8-ball? [Y]es / [N]o" + "\n");
+
+            // Get user input to continue game
+            Scanner response = new Scanner(System.in);
+            System.out.println("You have responded to " + response);
+
+            //if-else statements for user input and checking if valid
+        }
     }
 }
